953a954c16e4327a5cee9d44281c1fcd21d1aa03
Riley Childs <me@rileychilds.me>
2019-07-21 21:13:39 -0700

    add coded leader

--- src/events.js
+++ src/events.js
@@ -169,7 +169,7 @@ const sayThankyou = ( event ) => {
 const sendHelp = ( event ) => {
 
   const botUserID = helpers.extractUserID( event.text );
-
+  var today = new Date();
   const crypto = require('crypto')
   , shasum = crypto.createHash('sha1');
   shasum.update(event.user + today.getHours() + today.getMinutes() + today.getFullYear() + today.getMonth() + today.getDate());
