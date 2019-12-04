dc9a4821e2e1f77e6b5ab964833a6799d12d8fe2
Peter <16809252+percula@users.noreply.github.com>
2019-12-04 08:39:01 -0500

    Capitalize test user

--- src/events.js
+++ src/events.js
@@ -53,7 +53,7 @@ const handlePlusMinus = async( item, operation, quantity, channel, userInit, ts 
    const score = await points.updateScore( item, operation, quantity ),
         operationName = operations.getOperationName( operation ),
         message = messages.getRandomMessage( operationName, item, score );
-        if (item == "test") {
+        if (item == "TEST") {
           return slack.sendThreadMessage( message, channel, ts );
         } else {
           return slack.sendMessage( message, channel );
